d7ad3ebe6426a0019cc270b2334bdcc0aacdebaf
jennids <90724018+jennids@users.noreply.github.com>
2021-11-02 14:45:55 -0400

    update5

--- Platformer/Scenes/Menu.gd
+++ Platformer/Scenes/Menu.gd
@@ -2,3 +2,11 @@ extends Node2D
 
 func _on_Play_pressed():
 	get_tree().change_scene("res://Levels/Level2.tscn")
+
+
+func _on_How_to_Play_pressed():
+	get_tree().change_scene("res://Scenes/HowToPlay.tscn")
+
+
+func _on_Artist_Statement_pressed():
+	get_tree().change_scene("res://Scenes/ArtistStatement.tscn")
